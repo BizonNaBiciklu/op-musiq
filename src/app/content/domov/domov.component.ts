3cb518aadc7768e343b842b45a0f4ab668c5be56
priprave05 <priprave05@fri1.uni-lj.si>
2018-07-04 13:46:29 +0200

    Work on homepage

--- src/app/content/domov/domov.component.ts
+++ src/app/content/domov/domov.component.ts
@@ -6,6 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./domov.component.css']
 })
 export class DomovComponent implements OnInit {
+  showcaseCards: any = [
+    {
+      title: "Slow",
+      text: "As slow as a music player can get.",
+      img: ""
+    },
+    {
+      title: "Ugly",
+      text: "Because who looks at a music player while the music is playing anyway?",
+      img: ""
+    },
+    {
+      title: "Works",
+      text: "We ran out of stupid promo ideas, just leave us alone, ok?",
+      img: ""
+    }
+  ];
 
   constructor() { }
 
