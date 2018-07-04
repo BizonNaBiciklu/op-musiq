531b96a382f87c1f14956d2076f7cd3c78c3b373
priprave05 <priprave05@fri1.uni-lj.si>
2018-07-04 14:21:32 +0200

    Home screen almost done

--- src/app/content/domov/domov.component.ts
+++ src/app/content/domov/domov.component.ts
@@ -8,19 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class DomovComponent implements OnInit {
   showcaseCards: any = [
     {
-      title: "Slow",
-      text: "As slow as a music player can get.",
-      img: ""
+      title: "Big selection",
+      text: "Whole Youtube is your playground. You can listen to any music or other video found there.",
+      img: "fas fa-podcast"
     },
     {
-      title: "Ugly",
-      text: "Because who looks at a music player while the music is playing anyway?",
-      img: ""
+      title: "Listen with others",
+      text: "Take turns with other users on who selects the music.",
+      img: "fas fa-user-friends"
     },
     {
-      title: "Works",
-      text: "We ran out of stupid promo ideas, just leave us alone, ok?",
-      img: ""
+      title: "Chat",
+      text: "Chat with others while listening. Share your love for a song, or just frown upon how garbage current track is.",
+      img: "far fa-comment-alt"
     }
   ];
 
